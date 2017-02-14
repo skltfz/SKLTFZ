ddb54901e3276d82f06af13d03d4030acb52339a
sam.mak <sam.mak@LBHWPC0057>
2017-02-14 13:01:37 +0800

    Add project files.

---
+++ SKLTFZ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SKLTFZ.Startup))]
+namespace SKLTFZ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
