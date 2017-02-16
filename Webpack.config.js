f517c13b0475f2bf9956cfd8c73e87473d093e3a
rubbish <sam.mak@LBHWPC0057>
2017-02-16 12:20:18 +0800

    GOD - INIT

---
+++ Webpack.config.js
@@ -0,0 +1,89 @@
+﻿const webpack = require("webpack");
+const WebpackNotifierPlugin = require('webpack-notifier');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const DIST_FOLDER = 'dist';
+
+module.exports = {
+	entry: './Scripts/app/App.tsx',
+	output: {
+		filename: 'Scripts/app/bundle.js'
+	},
+	resolve: {
+        extensions: ['', '.Webpack.js', '.web.js', '.ts', '.js', '.tsx']
+	},
+	plugins: [
+        //new ExtractTextPlugin('vendor.css', {
+        //    allChunks: true,
+        //    disable: NODE_ENV !== 'production'
+        //}),
+        //new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.js"),
+        //new webpack.ProvidePlugin({
+        //    //$: 'signalr',
+        //    //$: 'jquery',
+        //    //$: 'jquery-validation',
+        //    //jQuery: 'jquery'
+        //    _: 'lodash',
+        //    moment: 'moment',
+        //    math: 'mathjs',
+        //    html2canvas: "html2canvas",
+        //}),
+        //new webpack.DefinePlugin({
+        //    'process.env': {
+        //        'NODE_ENV': JSON.stringify(NODE_ENV)
+        //    }
+        //}),
+        //new webpack.HotModuleReplacementPlugin(),    // for dev
+        //new webpack.NoErrorsPlugin(),
+        //new WebpackNotifierPlugin(),
+        //new webpack.DllReferencePlugin({
+        //    context: __dirname,
+        //    manifest: require('./Scripts/dll/manifest.json'),
+        //}),
+        new CleanWebpackPlugin([DIST_FOLDER], {
+            "verbose": true
+        })
+	],
+    devtool: 'source-map',
+	//devtool: 'cheap-module-eval-source-map',
+    //devtool: 'inline-eval-cheap-source-map',
+    module: {        
+	    //preLoaders: [
+        //    { test: /\.ts(x?)$/, loader: "tslint", exclude: [/api.ts/] }
+	    //],
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                exclude:    /(node_modules|bower_components)/,
+                loader:'ts-loader'
+            },
+            //{ test: /\.css$/, loader: "style!css" },
+            //{ test: /\.ts(x?)$/, loader: 'ts-loader' }  // for production
+            {
+                test: /\.ts(x?)$/,
+                loaders: ['react-hot', 'awesome-typescript-loader'],
+                include: path.join(__dirname, '/TypeScripts/src')
+            },
+            {
+                test: /\.js(x?)$/,
+                loaders: ['react-hot', 'awesome-typescript-loader?doTypeCheck=false&useBabel=true&useWebpackText=true'],
+                include: path.join(__dirname, '/TypeScripts/src')
+            },
+            //{ test: /\.tsx?$/, loader: 'ts-loader?compiler=ntypescript' }
+            {
+                include: /\.json$/,
+                loaders: ["json-loader"]
+            },
+            //{
+            //    test: /\.css$/,
+            //    loader: ExtractTextPlugin.extract(
+            //        'style-loader',
+            //        //'css-loader?modules&importLoaders=1&localIdentName=[local]!postcss-loader'
+            //        'css-loader!postcss-loader'
+            //    ),
+            //},
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
+	    ]
+	},
+}
+console.log("am i here?");
---
+++ relay.js
@@ -0,0 +1,38 @@
+﻿console.log("zz");
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./Webpack.config');
+
+
+//new WebpackDevServer(webpack(config), {
+//    // https://webpack.github.io/docs/webpack-dev-server.html
+//    publicPath: config.output.publicPath,
+//    hot: true,
+//    historyApiFallback: true,
+//    quiet: false,//???
+//    noInfo: false,//???
+//    // https://webpack.js.org/configuration/stats/
+//    stats: {
+//        colors: true,
+//        hash: false,
+//        version: false,
+//        timings: true,
+//        assets: true,
+//        chunks: false,
+//        modules: false,
+//        reasons: true,//???
+//        children: true,//???
+//        source: false,
+//        errors: true,
+//        errorDetails: true,
+//        warnings: true,
+//        publicPath: false,
+//        cached: false
+//    }
+//}).listen(config.output.port, 'localhost', function (err, result) {
+//    if (err) {
+//        return console.log(err);
+//    }
+
+//    console.log(`Listening at http://localhost:${config.output.port}/`);
+//});
